89c85ac89d0182ceaca130aa2efe5d244e81f8e2
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2023-01-06 16:43:33 +0700

    Add docstring for each jarvis interaction function

--- response.py
+++ response.py
@@ -12,6 +12,10 @@ from voice_utils import speak, speech_reconnize
 
 def wish_me() -> None:
     
+    '''
+        Speak a greeting based on the current hour.
+    '''
+    
     hour = dt.datetime.now().hour
     
     if hour in range(0, 4):
@@ -38,45 +42,92 @@ def wish_me() -> None:
     
 
 def jarvis_speech() -> None:
+    
+    '''
+        Speak a greeting.
+    '''
+    
     speak(engine, "At Your Service Sir, How can I help you")
     
 
 def tell_joke(list_of_jokes: list[str]) -> None:
+    
+    '''
+        Tell a random joke from the given list.
+        Parameters:
+        list_of_jokes (list[str]): A list of strings, where each string is a joke.
+    '''
+    
     speak(engine, random.choice(list_of_jokes))
     
     
 ##### Open website #####
     
 def open_google() -> None:
+    
+    '''
+        Open the Google website in the default web browser.
+    '''
+
     speak(engine, "Here We Go")
     wb.open("google.com")
 
 
 def open_instragram() -> None:
+    
+    '''
+        Open the Instagram website in the default web browser.
+    '''
+
     speak(engine, "Here We Go")
     wb.open("instagram.com")
     
     
 def open_facebook() -> None:
+    
+    '''
+        Open the Facebook website in the default web browser.
+    '''
+
     speak(engine, "Here We Go")
     wb.open("facebook.com")
     
     
 def open_spotify() -> None:
+    
+    '''
+        Open the Spotify website in the default web browser.
+    '''
+
     speak(engine, "Opening spotify")
     wb.open("spotify.com")
     
     
 def open_twitter() -> None:
+    
+    '''
+        Open the Twitter website in the default web browser.
+    '''
+
     speak(engine, "Here We Go")
     wb.open("twitter.com")
     
     
 def open_youtube() -> None:
+    
+    '''
+        Open the YouTube website in the default web browser.
+    '''
+
     speak(engine, "Here We Go")
     wb.open("youtube.com")
 
 def open_youtube_video_converter() -> None:
+    
+    '''
+        Open the YouTube video converter website in the default web browser.
+    '''
+
     speak(engine, "Here We Go")
     wb.open("en.onlinevideoconverter.pro")
 
@@ -85,6 +136,10 @@ def open_youtube_video_converter() -> None:
 
 def google_search() -> None:
     
+    '''
+        Recognize speech from the microphone and perform a Google search using the recognized text.
+    '''
+
     speak(engine, "What Should I Search Sir ?")
     
     search_data = speech_reconnize().replace(" ", "+")
@@ -95,6 +150,10 @@ def google_search() -> None:
     
 def wikipedia_search() -> None:
     
+    '''
+        Recognize speech from the microphone and perform a Wikipedia search using the recognized text.
+    '''
+
     speak(engine, 'Searching in Wikipedia')
     
     query.replace("according to wikipedia", "")
@@ -108,6 +167,10 @@ def wikipedia_search() -> None:
     
 def youtube_search() -> None:
     
+    '''
+        Recognize speech from the microphone and perform a YouTube search using the recognized text.
+    '''
+
     speak(engine, "What Should I Search Sir?")
     search_yt = speech_reconnize().replace(" ", "+")
     
@@ -118,11 +181,21 @@ def youtube_search() -> None:
 ##### Date time #####    
     
 def get_time() -> None:
+    
+    '''
+        Tell the current time.
+    '''
+    
     strTime = dt.datetime.now().strftime("%H:%M:%S")
     speak(engine, strTime)
     
     
 def get_date() -> None:
+    
+    '''
+        Tell the current date.
+    '''
+    
     today = dt.date.today()
     speak(engine, today)
     
@@ -160,6 +233,10 @@ def mute() -> None:
 
 def set_brightness() -> None:
     
+    '''
+        Recognize speech from the microphone and set the screen brightness to the recognized value.
+    '''
+    
     try:
         
         bright = int(speech_reconnize())
@@ -177,6 +254,10 @@ def set_brightness() -> None:
         
 ##### Todo #####
 def todo() -> None:
+    
+    '''
+        Add, read, or clear tasks from a to-do list.
+    '''
     
     if "add" in query or "create" in query:
         
