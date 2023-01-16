9b75771dcabb9d3b6ef3d85ec83e0d2e906a7dcf
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2023-01-16 10:53:20 +0700

    Rewrite bytes_to_mb

--- jarvis.py
+++ jarvis.py
@@ -42,9 +42,7 @@ def speak(audio):  # speak audio
 
 
 def bytes_to_mb(bytes):
-    KB = 1024  # One Kilobyte is 1024 bytes
-    MB = KB * 1024  # One MB is 1024 KB
-    return int(bytes / MB)
+    return int(bytes / (1024 ** 2))
 
 
 def wishMe():  # wishes me
